8a3c89333fd79220a03192cba3e8bd4a65ba7ed3
unknown <julianaruizh@gmail.com>
2025-10-04 22:20:55 -0400

    Lab5 done

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,42 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    result = ""
+
+    for i in range (n):
+        if i == (n-1) or i == 0:
+            result += "*" *n + "\n"
+        else:
+            result += "*" + " " * (n-2) + "*" + "\n"
+    
+    return result.strip()
+
+print(hollow_square(5))
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ""
 
+    for i in range (n):
+        for j in range (1, i+2):
+            result += str(j)
+        result += "\n"
+    
+    return result.strip()
+
+print(number_pattern(5))
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    reuslt = ""
+
+    sum = 0
+    for i in range (n+1):
+        sum += i
+    return sum
+print(sum_of_natural_numbers(5))
 
 # Example for n = 4:
 #    *
@@ -24,4 +48,13 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ""
+
+    for i in range(n):
+        spaces = n - i -1
+        stars = 2 * i + 1
+
+        result += " " * spaces + "*" * stars + "\n"
+
+    return result.rstrip()
+print(centered_star_pyramid(4))
